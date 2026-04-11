673051af3bf62ab5310c3e976afd347e72db0116
Amin Avan <aminavan.hardware@yahoo.com>
2026-04-11 15:49:46 -0400

    https://leetcode.com/problems/1-bit-and-2-bit-characters

--- Bit manipulation and Binary ops/1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
+++ Bit manipulation and Binary ops/1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -1,51 +1,20 @@
 #include <iostream>
 #include <vector>
 
-class Solution{
+class Solution {
 public:
     bool isOneBitCharacter(std::vector<int>& bits) {
-        bool is_oneBit = false;
-        int i = 1;
-
-        if ((bits[0] == 0) && (1 == bits.size())){
-            is_oneBit = true;
+        int i = 0;
+        int n = bits.size();
+        while (i < (n - 1)){
+            if (bits[i] == 1){
+                i = i + 2;
+            } else {
+                i = i + 1;
+            }
         }
 
-        while ((i < bits.size()) && (1 < bits.size())){
-            if ((bits[i] == 1) && (bits[i-1] == 1)){
-                if ((i + 2) >= bits.size()){
-                    if (bits[i+1] == 0){
-                        is_oneBit = true;
-                        return is_oneBit;
-                    } else {
-                        i = i + 1;
-                    }
-                    // std::cout << "11 --> i: " << i << ", size: " << ((bits.size())) << std::endl;
-                } else if ((i + 2) < bits.size()){
-                    i = i + 2;
-                    // std::cout << "11 --> i: " << i << ", size: " << ((bits.size())) << std::endl;
-                }                
-            } else if ((bits[i] == 0) && (bits[i-1] == 1)){
-                if ((i + 2) >= bits.size()){
-                    if (bits[i+1] == 0){
-                        is_oneBit = true;
-                        return is_oneBit;
-                    } else {
-                        i = i + 1;
-                    }
-                    // std::cout << "10 --> i: " << i << ", size: " << ((bits.size())) << std::endl;
-                } else if ((i + 2) < bits.size()) {
-                    i = i + 2;
-                    // std::cout << "10 --> i: " << i << ", size: " << ((bits.size())) << std::endl;
-                }
-            } else if ((bits[i] == 0)){
-                is_oneBit = true;
-                i = i + 1;
-                // std::cout << "0 --> i: " << i << ", size: " << ((bits.size())) << std::endl;
-            }
-        }        
-
-        return is_oneBit;
+        return i == n - 1;
     }
 };
 
@@ -57,9 +26,9 @@ int main(){
     // input
     // std::vector<int> input = {1,1,1,1,1,1,0}; // true
     // std::vector<int> input = {1,0,0}; // true
-    std::vector<int> input = {1,1,1,0}; // false
+    // std::vector<int> input = {1,1,1,0}; // false
     // std::vector<int> input = {0}; // true
-    // std::vector<int> input = {0,0}; // true
+    std::vector<int> input = {0,0}; // true
 
     // output
     std::cout << my_code.isOneBitCharacter(input) << std::endl;
