e0f922f3bad236ecc21eb5577a3f5ebd1628563f
Amin Avan <aminavan.hardware@yahoo.com>
2026-04-11 19:39:13 -0400

    EXAMPLE: https://www.hackerrank.com/challenges/c-tutorial-pointer/problem

---
+++ Memory layout and pointers and structs/intro/intro.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+void increment(int *v){
+    (*v)++;
+}
+
+int main(){
+
+    int a;
+    scanf("%d", &a);
+
+    increment(&a);
+    printf("%d", a);
+
+    
+    return 0;
+}
